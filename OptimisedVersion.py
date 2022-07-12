f3b027bb4483662592bcb14522fc6dbaa445c4bd
Jeevan Roy <iitroy00@gmail.com>
2022-07-12 13:22:33 +0530

    An acceptable optimised version

--- OptimisedVersion.py
+++ OptimisedVersion.py
@@ -1,13 +1,48 @@
 '''
-OPTIMISED VERSION of the finalrough.py file.
-That can generate rectangles without compromising the space in the 1000x1000 pagesize.
+    #This an acceptable code that generates n number rectangles that do not overlap on each other.
 
-Try n=98,99,100,101,102 in both the finalrough.py and this file.
-You can notice the optimization.
+    #This is an optimised version where we can optimise the space on the 1000x1000 page.
 
-Explanation:
-    According to the finalrough.py file you are saying that n = even, odd, perfect_square, odd, and even.
-    But According to this file you are asking for n=100-2,100-1,100,121-20,121-19.
+    #Removing the perfect_square() check and perfect_square_rect() functions and using only one function that contains the break_value variable
+    and renaming that to generate_rectangles()
+
+    #In the optimised one if we provide n=9 then a 3x3 grid will be generated and 9 rectangles will be generated.
+
+    #But now for n=9 a 4x4 grid will be generated and 9 rectangles are drawn leaving the other 7.
+    And for n=15 a 4x4 grid will be generated and 15 rectangles are drawn leaving the ONE.
+
+    #Rectangles are drawn like the numbers in the numberpad on our keyboard.
+    Ex: for n=9;
+    row1:1st rectangle will be generated at number 1 block,then 2,then 3,then4 ,and then row2 starts filling with the 4th on top of 1st....
+
+    #Here space on the page is not optimised.
+
+    #for every n there will be a k that satisfies k**2==n and if we provide n then instead of kxk grid (k+1)x(k+1) grid will be generated.
+
+    #resulting a maximum of '(2k+1)' rectangles space being unoptimised and a minimum of 'ONE' rectangle space being unoptimised.
+
+    Ex: for n=9 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 7(max 2k+1) rectangles space being unoptimised
+    i.e., (2*3+1).
+    Ex: for n=10 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 6 rectangles space being unoptimised
+    i.e., ((2*3+1)-1).
+    Ex: for n=11 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 5 rectangles space being unoptimised
+    i.e., ((2*3+1)-2).
+    Ex: for n=12 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 4 rectangles space being unoptimised
+    i.e., ((2*3+1)-3).
+    Ex: for n=13 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 3 rectangles space being unoptimised
+    i.e., ((2*3+1)-4).
+    Ex: for n=14 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 2 rectangles space being unoptimised
+    i.e., ((2*3+1)-5).
+    Ex: for n=15 we have k=3 i.e., 3**2==9 then 4x4 grid will be generated. resulting a maximum of 1(min 1) rectangles space being unoptimised
+    i.e., ((2*3+1)-6).
+    Ex: for n=16 we have k=4 i.e., 4**2==16 then 5x5 grid will be generated. resulting a maximum of 9(max 2k+1) rectangles space being unoptimised
+    i.e., (2*4+1).
+
+    #In the previous one we got maximum of 2k and minimum of 1 rectangle space being unoptimised.
+    #But in this below code
+                     we got a maximum of (2k+1) and minimum of 1 rectangle space being unoptimised.
+
+        Which is completely acceptable where the difference between the max unoptimised rectangles is ONE.
 
 '''
 
@@ -18,97 +53,7 @@ n = int(input("How many number of rectangles do you need?: "))
 
 c = canvas.Canvas("pdf_table{4}.pdf", pagesize=(1000,1000))
 
-
-def perfect_square(n):
-
-    '''
-    #a function to check whether n is a perfect square or not.
-    #The below function returns True if n is a perfect sqauare and False if n is not a perfect square.
-    '''
-
-    i=1
-    while (i<n+1):
-        if i**2==n:
-            global i_val
-            i_val = i
-            return True
-        i+=1
-    else:
-        return False
-
-
-def perfect_square_rect():
-    '''
-    #The below function is called when n is a percect square of some number.
-    #A function to generate k by k grid and generate rectangles in them that did not overlap each other.
-    '''
-
-    x=[' ']*(k+1)
-    k_part = 0
-
-    while (k_part<k+1):
-        x[k_part]=int(k_part*(1000/k))
-        k_part+=1
-    print('.'*100)
-    print('.'*100)
-    print('Vertical Lines Which Are Rectangles Borders',x)
-    y = x
-
-    xy_tups = []
-    ci=0
-    while (ci<len(x)-1):
-        if ci==(len(x)-1):
-            xi = (x[ci-1]+x[ci])/2
-            yi = (y[ci-1]+y[ci])/2
-            xy_tups.append((int(xi),int(yi)))
-        else:
-            xi = (x[ci]+x[ci+1])/2
-            yi = (y[ci]+y[ci+1])/2
-            xy_tups.append((int(xi),int(yi)))
-
-        ci+=1
-
-    x_center = []
-    for ele in xy_tups:
-        x_center.append(ele[0])
-
-    y_center = []
-    for ele in xy_tups:
-        y_center.append(ele[1])
-
-
-    print('-'*100)
-    print('-'*100)
-    print("x center:" ,x_center)
-    print('-'*100)
-    print('-'*100)
-    print("y center",y_center)
-
-
-    main_coords = []
-    for ele_x in x_center:
-        for ele_y in y_center:
-             w = random.randrange(0,int(1000/k))
-             h = random.randrange(0,int(1000/k))
-             c.rect(ele_x,ele_y,w,h)
-             main_coords.append((ele_x,ele_y,w,h))
-    print('-'*100)
-    print('-'*100)
-    print('MAIN COORDINATES LIST',main_coords)
-    print('_'*100)
-    print('_'*100)
-    print('Length of the main_coords list',len(main_coords))
-    print('-'*100)
-    print('-'*100)
-
-
-    c.save()
-    print("Pdf generated....")
-    print('.'*100)
-    print('.'*100)
-
-
-def not_a_perfect_square():
+def generate_rectangles():
     '''
     #This function is called when n is NOT a percect square of some number.
     #A function to generate n number of rectangles.
@@ -193,25 +138,6 @@ def not_a_perfect_square():
     print('.'*100)
     print('.'*100)
 
-
-#if n is a perfect square then run the "if block" otherwise run the "else block"
-#if n is a perfect square k by k grid will be gerated where k**2==n Ex: n=100 then k=10
-#if n is NOT a perfect square then k by k grid will be generated where (k**2)-(constant)==n
-#Ex: i) n=98 then k=10 and constant = 2 -> (10**2)-2==98
-#Ex: i) n=99 then k=10 and constant = 1 -> (10**2)-1==99
-#Ex: i) n=100 then k=10 PERFECT SQUARE i.e., 10**2==100
-#Ex: i) n=101 then k=11 and constant = 20 -> (11**2)-20==101
-#Ex: i) n=102 then k=11 and constant = 19 -> (11**2)-19==102
-
-if False: #perfect_square(n):
-    k=i_val
-    perfect_square_rect()
-    #print(i_val)
-
-else:
-    i_list = [i for i in range(int(n**0.5+1))]
-    i_val = i_list[-1]+1
-
-    #print(i_val)
-    k=i_val
-    not_a_perfect_square()
+i_val = [i for i in range(int(n**0.5+1))][-1]+1
+k=i_val
+not_a_perfect_square()
